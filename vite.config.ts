e5a39696c551204f4c4d24156b1ab818ea6ae791
FuBaooo <baihe2233@gmail.com>
2022-06-08 10:29:29 +0800

    feat: config change

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
       extendRoute(route) {
         const path = resolve(__dirname, route.component.slice(1))
 
-        if (!path.includes('projects.md')) {
+        if (!path.includes('projects-')) {
           const md = fs.readFileSync(path, 'utf-8')
           const { data } = matter(md)
           route.meta = Object.assign(route.meta || {}, { frontmatter: data })
