571cab7b2dd37f62f30494609aeacc02d2743fd1
FuBaooo <baihe2233@gmail.com>
2022-06-13 10:22:33 +0800

    fix: switch components when switching languages

--- src/components/WrapLanguage.vue
+++ src/components/WrapLanguage.vue
@@ -5,12 +5,16 @@ const props = defineProps<{ path: string }>()
 
 const { locale } = useI18n()
 const router = useRouter()
-const localeIndex = shallowRef<DefineComponent | null>(null)
-const pageRoute = router.getRoutes().find(i => i.path === `${props.path}.${locale.value}`)
-if (pageRoute) {
-  const component = await (pageRoute.components.default as Function)()
-  localeIndex.value = component.default
+const localeIndex = ref<DefineComponent | null>(null)
+const pickLocaleComponent = async () => {
+  const pageRoute = router.getRoutes().find(i => i.path === `${props.path}.${locale.value.toLocaleLowerCase()}`)
+  if (pageRoute) {
+    const component = await (pageRoute.components.default as Function)()
+    localeIndex.value = markRaw(component.default)
+  }
 }
+await pickLocaleComponent()
+watch(() => locale.value, pickLocaleComponent)
 </script>
 
 <template>
