07a5855dbf9d661684e6ced8c3ff176fd243e442
FuBaooo <baihe2233@gmail.com>
2022-06-17 17:25:20 +0800

    feat: animate change in dark mode

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -3,8 +3,23 @@ import { isDark } from '~/composables/dark'
 </script>
 
 <template>
-  <TransitionGroup>
+  <TransitionGroup name="fade">
     <SolarSystem v-if="isDark" key="solar" />
     <Galaxy v-show="!isDark" key="galaxy" />
   </TransitionGroup>
 </template>
+
+<style>
+.fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.fade-leave-active {
+  transition: all 0.3s ease-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
