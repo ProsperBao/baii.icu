27f6233dd834b601a8c3d10bc2a1c1a307fc48d5
FuBaooo <baihe2233@gmail.com>
2022-08-18 17:13:52 +0800

    fix: type

--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -7,7 +7,7 @@ import { type UserModule } from '~/types'
 // Don't need this? Try vitesse-lite: https://github.com/antfu/
 const messages = Object.fromEntries(
   Object.entries(
-    import.meta.glob<true, string, { default: Record<string, any> }>('../../locales/*.y(a)?ml', { eager: true }))
+    import.meta.globEager('../../locales/*.y(a)?ml'))
     .map(([key, value]) => {
       const yaml = key.endsWith('.yaml')
       return [key.slice(14, yaml ? -5 : -4), value.default]
